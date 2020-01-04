51c5f0fabc9ee8e9a13402595398af475a8d8e99
cjihrig <cjihrig@gmail.com>
2020-01-03 19:46:27 -0500

    remove `code` dependency

--- test/index.js
+++ test/index.js
@@ -1,5 +1,5 @@
 'use strict';
-const Code = require('code');
+const Assert = require('assert');
 const Hapi = require('hapi');
 const Lab = require('lab');
 const Timi = require('../lib');
@@ -7,7 +7,6 @@ const Timi = require('../lib');
 // Test shortcuts
 const lab = exports.lab = Lab.script();
 const { describe, it } = lab;
-const { expect } = Code;
 
 
 async function getServer () {
@@ -51,34 +50,38 @@ describe('Server Timing', () => {
   it('returns basic server timing info', async () => {
     const server = await getServer();
     const res = await server.inject({ method: 'GET', url: '/happy' });
+    const regex = /^total;dur=.+;desc="Total",auth;dur=.+;desc="Authentication",handler;dur=.+;desc="Handler"$/;
 
-    expect(res.headers['server-timing']).to.match(/^total;dur=.+;desc="Total",auth;dur=.+;desc="Authentication",handler;dur=.+;desc="Handler"$/);
-    expect(res.statusCode).to.equal(200);
-    expect(res.payload).to.equal(JSON.stringify({ foo: 'bar' }));
+    Assert.strictEqual(regex.test(res.headers['server-timing']), true);
+    Assert.strictEqual(res.statusCode, 200);
+    Assert.strictEqual(res.payload, JSON.stringify({ foo: 'bar' }));
   });
 
   it('works with custom timings', async () => {
     const server = await getServer();
     const res = await server.inject({ method: 'GET', url: '/custom-timings' });
+    const regex = /^total;dur=.+;desc="Total",auth;dur=.+;desc="Authentication",handler;dur=.+;desc="Handler",miss,region;desc="us-east-1",db;dur=.+$/;
 
-    expect(res.headers['server-timing']).to.match(/^total;dur=.+;desc="Total",auth;dur=.+;desc="Authentication",handler;dur=.+;desc="Handler",miss,region;desc="us-east-1",db;dur=.+$/);
-    expect(res.statusCode).to.equal(200);
-    expect(res.payload).to.equal(JSON.stringify({ bar: 'baz' }));
+    Assert.strictEqual(regex.test(res.headers['server-timing']), true);
+    Assert.strictEqual(res.statusCode, 200);
+    Assert.strictEqual(res.payload, JSON.stringify({ bar: 'baz' }));
   });
 
   it('works with a 404', async () => {
     const server = await getServer();
     const res = await server.inject({ method: 'GET', url: '/not-found' });
+    const regex = /^total;dur=.+;desc="Total"$/;
 
-    expect(res.headers['server-timing']).to.match(/^total;dur=.+;desc="Total"$/);
-    expect(res.statusCode).to.equal(404);
+    Assert.strictEqual(regex.test(res.headers['server-timing']), true);
+    Assert.strictEqual(res.statusCode, 404);
   });
 
   it('throws when ending a timing that does not exist', async () => {
     const server = await getServer();
     const res = await server.inject({ method: 'GET', url: '/end-missing-entry' });
+    const regex = /^total;dur=.+;desc="Total",auth;dur=.+;desc="Authentication",handler;desc="Handler"$/;
 
-    expect(res.headers['server-timing']).to.match(/^total;dur=.+;desc="Total",auth;dur=.+;desc="Authentication",handler;desc="Handler"$/);
-    expect(res.statusCode).to.equal(500);
+    Assert.strictEqual(regex.test(res.headers['server-timing']), true);
+    Assert.strictEqual(res.statusCode, 500);
   });
 });
